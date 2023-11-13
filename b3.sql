bc506206a272114ecbfffd60b99f648c321b6da8
Leonardo C. Junior <leonardo.cjr@hotmail.com>
2023-11-13 16:21:51 -0200

    Update b3.sql

--- b3.sql
+++ b3.sql
@@ -3,14 +3,12 @@ CREATE SCHEMA b3
 USE b3
 
 # RN 01
-
 CREATe TABLE setor(
 	set_id INT PRIMARY KEY,
     set_nome VARCHAR(45) NOT NULL,
 );
 
 # RN 02
-
 CREATE TABLE subsetor(
 	sub_id INT 
 
@@ -18,34 +16,26 @@ CREATE TABLE subsetor(
 
 # RN 03
 
-sdkgfjsdkgdsjgdskj
 
 # RN 04
-
 
 
 # RN 05
 
 
-
 # RN 06
-
 
 
 # RN 07
 
 
-
 # RN 08
-
 
 
 # RN 09
 
 
-
 # RN 10
-
 
 
 # RN 11
