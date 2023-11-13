a78dda9b02bceb31925e1286edeb47a7b641a86e
Leonardo C. Junior <leonardo.cjr@hotmail.com>
2023-11-13 16:14:47 -0200

    RN 01 criada

---
+++ b3.sql
@@ -0,0 +1,51 @@
+CREATE SCHEMA b3
+
+USE b3
+
+# RN 01
+
+CREATe TABLE setor(
+	set_id INT PRIMARY KEY,
+    set_nome VARCHAR(45) NOT NULL,
+);
+
+# RN 02
+
+CREATE TABLE subsetor(
+	sub_id INT 
+
+);
+
+# RN 03
+
+
+
+# RN 04
+
+
+
+# RN 05
+
+
+
+# RN 06
+
+
+
+# RN 07
+
+
+
+# RN 08
+
+
+
+# RN 09
+
+
+
+# RN 10
+
+
+
+# RN 11
