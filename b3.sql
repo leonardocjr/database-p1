948390213d9708ca79918cefc8a59612d6c1d744
Leonardo C. Junior <leonardo.cjr@hotmail.com>
2023-11-13 22:49:58 -0300

    Atualização final 1

--- b3.sql
+++ b3.sql
@@ -3,85 +3,105 @@ CREATE SCHEMA b3;
 USE b3;
 
 # RN 01
-CREATe TABLE setor (
-	setor_id VARCHAR(10) NOT NULL PRIMARY KEY,
+CREATE TABLE setor (
+	setor_id INT PRIMARY KEY,
     setor_nome VARCHAR(100) NOT NULL
 );
 
 # RN 02
 CREATE TABLE subsetor (
-	sub_id VARCHAR(10) NOT NULL PRIMARY KEY,
-	sub_nome VARCHAR(100) NOT NULL,
-	setor_id VARCHAR(10) NOT NULL,
-	CONSTRAINT fk_subsetor_setor
-	FOREIGN KEY (setor_id) REFERENCES setor(setor_id)
+    sub_id INT PRIMARY KEY,
+    sub_nome VARCHAR(100) NOT NULL,
+    setor_id INT NOT NULL,
+    CONSTRAINT fk_subsetor_setor FOREIGN KEY (setor_id)
+        REFERENCES setor (setor_id)
 );
 
 # RN 03
 CREATE TABLE segmento (
-	seg_id VARCHAR(10) NOT NULL PRIMARY KEY,
-	seg_nome VARCHAR(100) NOT NULL,
-	sub_id VARCHAR(10) NOT NULL,
-	CONSTRAINT fk_segmento_subsetor
-	FOREIGN KEY (sub_id) REFERENCES subsetor(sub_id)
+    seg_id VARCHAR(3) PRIMARY KEY,
+    seg_nome VARCHAR(100) NOT NULL,
+    sub_id INT NOT NULL,
+    CONSTRAINT fk_segmento_subsetor FOREIGN KEY (sub_id)
+        REFERENCES subsetor (sub_id)
 );
 
 # RN 04
 CREATE TABLE governanca (
-	gov_id VARCHAR(10) NOT NULL PRIMARY KEY,
+	gov_id VARCHAR(3) PRIMARY KEY,
 	gov_nome VARCHAR(100) NOT NULL
 );
 
 # RN 05
 CREATE TABLE empresa (
-	emp_id VARCHAR(10) NOT NULL PRIMARY KEY,
+	emp_id VARCHAR(5) PRIMARY KEY,
 	emp_nome VARCHAR(100) NOT NULL,
-	gov_id VARCHAR(10),
+	gov_id VARCHAR(3),
 	CONSTRAINT fk_empresa_governanca
 	FOREIGN KEY (gov_id) REFERENCES governanca(gov_id),
-	seg_id VARCHAR(10) NOT NULL,
+	seg_id VARCHAR(3) NOT NULL,
 	CONSTRAINT fk_empresa_segmento
 	FOREIGN KEY (seg_id) REFERENCES segmento(seg_id)
-)
+);
 
-# RN 06
+# RN 06 c
 CREATE TABLE acao (
-	acao_id VARCHAR(10) NOT NULL PRIMARY KEY,
-	emp_id VARCHAR(10) NOT NULL,
-	CONSTRAINT fk_acao_empresa
-	FOREIGN KEY (emp_id) REFERENCES empresa(emp_id)
-)
+    acao_id VARCHAR(6) PRIMARY KEY,
+    emp_id VARCHAR(5) NOT NULL,
+    CONSTRAINT fk_acao_empresa FOREIGN KEY (emp_id)
+        REFERENCES empresa (emp_id)
+);
 
 # RN 07
 CREATE TABLE tipo_indice (
-	tipo_id VARCHAR(10) NOT NULL PRIMARY KEY,
+	tipo_id INT PRIMARY KEY,
 	tipo_nome VARCHAR(100) NOT NULL
 );
 
 # RN 08
 CREATE TABLE indice (
-	indice_id VARCHAR(10) NOT NULL PRIMARY KEY,
-	indice_nome VARCHAR(100) NOT NULL,
-	tipo_id VARCHAR(10) NOT NULL,
-	CONSTRAINT fk_indice_tipo_indice
-	FOREIGN KEY (tipo_id) REFERENCES tipo_indice(tipo_id)
+    ind_id VARCHAR(6) PRIMARY KEY,
+    ind_nome VARCHAR(100) NOT NULL,
+    tipo_id INT NOT NULL,
+    CONSTRAINT fk_indice_tipo_indice FOREIGN KEY (tipo_id)
+        REFERENCES tipo_indice (tipo_id)
 );
 
 # RN 09
-CREATE TABLE carteira (
-	indice_id VARCHAR(10) NOT NULL PRIMARY KEY,
-	CONSTRAINT fk_carteira_indice
-	FOREIGN KEY (indice_id) REFERENCES indice(indice_id),
-	acao_id VARCHAR(10) NOT NULL PRIMARY KEY,
-	CONSTRAINT fk_carteira_empresa
-	FOREIGN KEY (acao_id) REFERENCES acao(acao_id),
-	qtdd_acoes INT(100000000000) NOT NULL,
-	percentual_acoes DECIMAL(6,3) NOT NULL,
-	quadrimestre INT(1) NOT NULL PRIMARY KEY,
-	ano INT(4) NOT NULL PRIMARY KEY
+CREATE TABLE listagem (
+	ind_id VARCHAR(6),
+	CONSTRAINT fk_listagem_indice FOREIGN KEY (ind_id) 
+		REFERENCES indice(ind_id),
+	acao_id VARCHAR(6),
+	CONSTRAINT fk_listagem_empresa FOREIGN KEY (acao_id) 
+		REFERENCES acao(acao_id),
+    list_quadrimestre INT CHECK(list_quadrimestre BETWEEN 1 AND 3),
+    list_ano YEAR,
+	list_qtdacoes INT NOT NULL,
+	list_percentual DECIMAL(6,3) CHECK(list_percentual BETWEEN 0.000 AND 100.000) NOT NULL,
+	PRIMARY KEY(ind_id, acao_id, list_quadrimestre, list_ano)
 );
 
 # RN 10
-
+CREATE TABLE mercado (
+	mer_id INT PRIMARY KEY,
+    mer_nome VARCHAR(100) NOT NULL
+);
 
 # RN 11
+CREATE TABLE cotacao (
+	cot_data DATE NOT NULL,
+    cot_id INT PRIMARY KEY,
+    cot_pabertura DECIMAL NOT NULL,
+    cot_pmax DECIMAL NOT NULL,
+    cot_pfechamento DECIMAL NOT NULL,
+    cot_qtdnegocios INT NOT NULL,
+    cot_qtdacoes INT NOT NULL,
+    cot_volumefin DECIMAL NOT NULL,
+	acao_id VARCHAR(6) NOT NULL,
+    CONSTRAINT fk_cotacao_acao FOREIGN KEY (acao_id)
+		REFERENCES acao(acao_id),
+	mer_id INT NOT NULL,
+    CONSTRAINT fk_cotacao_mercado FOREIGN KEY(mer_id)
+		REFERENCES mercado(mer_id)
+);
