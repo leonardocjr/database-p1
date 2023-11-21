804ba772009cea3ae7ff99c5676f35f966a58cb8
Mestre <edurdomestreg@gmail.com>
2023-11-21 16:26:18 -0200

    Finalizado GrupoTP-C_C5.sql

--- GrupoTP-C_C5.sql
+++ GrupoTP-C_C5.sql
@@ -1,10 +1,9 @@
-SELECT s.set_nome AS 'Nome do Setor Econômico', 
-       SUM(c.cot_volumefin) AS 'Volume Financeiro Total Gerado'
-FROM b3.cotacao c
-JOIN b3.acao a ON c.acao_id = a.acao_id
-JOIN b3.empresa e ON a.emp_id = e.emp_id
+﻿SELECT sub.sub_nome AS 'Subsetor Econômico', seg.seg_nome AS 'Segmento Ecônomico', e.emp_nome AS 'Pregão da Empresa'
+FROM b3.empresa e
 JOIN b3.segmento seg ON e.seg_id = seg.seg_id
+
 JOIN b3.subsetor sub ON seg.sub_id = sub.sub_id
+
 JOIN b3.setor s ON sub.set_id = s.set_id
-GROUP BY s.set_nome
-ORDER BY SUM(c.cot_volumefin) DESC;
+
+WHERE e.gov_id IS NULL AND s.set_nome = 'Tecnologia da Informação';
