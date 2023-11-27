878c75b8bba1c74e96feaf69aae923ae35acaed0
Leonardo C. Junior <leonardo.cjr@hotmail.com>
2023-11-26 21:55:47 -0300

    C2 atualizado ;)

--- GrupoTP-C_C2.sql
+++ GrupoTP-C_C2.sql
@@ -1,8 +1,24 @@
-SELECT i.ind_id AS 'Sigla do Índice', 
-       i.ind_nome AS 'Nome do Índice', 
-       l.acao_id AS 'Código da Ação', 
-       MAX(l.list_percentual) AS 'Percentual'
-FROM listagem l
-JOIN indice i ON l.ind_id = i.ind_id
-GROUP BY i.ind_id, i.ind_nome, l.acao_id
-ORDER BY MAX(l.list_percentual) DESC;
+SELECT 
+    indice.ind_id AS 'Índice',
+    indice.ind_nome AS 'Nome do Índice',
+    acao.acao_id AS 'Código da Ação',
+    listagem.list_percentual AS 'Percentual Máximo'
+FROM
+    listagem
+        JOIN
+    acao ON listagem.acao_id = acao.acao_id
+        JOIN
+    empresa ON acao.emp_id = empresa.emp_id
+        JOIN
+    indice ON listagem.ind_id = indice.ind_id
+WHERE
+    (empresa.emp_id , indice.ind_id, listagem.list_percentual) IN (SELECT 
+            empresa.emp_id, listagem.ind_id, MAX(list_percentual)
+        FROM
+            listagem
+                JOIN
+            acao ON listagem.acao_id = acao.acao_id
+                JOIN
+            empresa ON acao.emp_id = empresa.emp_id
+        GROUP BY empresa.emp_id , listagem.ind_id)
+ORDER BY listagem.list_percentual DESC;
